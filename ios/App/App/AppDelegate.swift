cc32637cde45a5193483091ec3bbdc311b227ac7
mikeinsgsummer <michaelinsg@yahoo.com>
2026-02-08 20:34:18 +0800

    Final sync: Update project config and app delegate for v1.1

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -23,7 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        #imageLiteral(resourceName: "simulator_screenshot_08A01C23-794C-4E30-A498-415282EFD276.png")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
